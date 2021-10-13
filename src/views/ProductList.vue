f7b9dce913fb1feeae2dc33c67e40ae5f471ab04
Luka Huang <codingluka@gmail.com>
2021-10-14 06:06:24 +0800

    add bootstrap to product

--- src/views/ProductList.vue
+++ src/views/ProductList.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="product-list">
     <h1>This is an product list</h1>
+    <button type="button" class="btn btn-primary">Primary</button>
+    <button type="button" class="btn btn-secondary">Secondary</button>
+    <button type="button" class="btn btn-success">Success</button>
+    <button type="button" class="btn btn-danger">Danger</button>
+    <button type="button" class="btn btn-warning">Warning</button>
+    <button type="button" class="btn btn-info">Info</button>
+    <button type="button" class="btn btn-light">Light</button>
+    <button type="button" class="btn btn-dark">Dark</button>
   </div>
 </template>
+<style lang="scss">
+  @import "~bootstrap/dist/css/bootstrap.css";
+</style>
